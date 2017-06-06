f69dd28bc5fbbe61c1c1bd9a63b0f0b8fe70296e
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2017-06-06 09:36:08 +0100

    minor fixes

--- rock_paper_scissors_ruby_sinatra/models/game.rb
+++ rock_paper_scissors_ruby_sinatra/models/game.rb
@@ -25,25 +25,19 @@ class Game
 
     if (@choice2 == "computer")
       @choice2 = computer_select()
-      # binding.pry
     end
     if (@choice1 == "scissors" && @choice2 == "paper")
       return "Player 1 wins!"
-    # end
     elsif (@choice1 == "paper" && @choice2 == "rock")
       return "Player 1 wins!"
-    # end
     elsif (@choice1 == "rock" && @choice2 == "scissors")
       return "Player 2 wins!"
-    # end
     elsif (@choice2 == "scissors" && @choice1 == "paper")
       return "Player 2 wins!"
-    # end
     elsif (@choice2 == "paper" && @choice1 == "rock")
       return "Player 2 wins!"
     elsif (@choice2 == "rock" && @choise1 == "scissors")
       return "Player 2 wins!"
-    # end
     elsif (@choice1 == "rock" && @choice2 == "rock" ||
         @choice1 == "paper" && @choice2 == "paper" ||
         @choice1 == "scissors" && @choice2 == "scissors")
@@ -51,16 +45,6 @@ class Game
     end
 
   end
-
-  # def return_option1_url(p1)
-  #   if (p1 == "rock")
-  #     return "rock"
-  #   elsif (p1 == "paper")
-  #     return "paper"
-  #   elsif (p1 == "scissors")
-  #     return "scissors"
-  #   end
-  # end
-  
+ 
 
 end
